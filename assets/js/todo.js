bf81008e84a7e9f636e942d2e9bd8d1de34dd355
Angie Condor <angiegita1602@gmail.com>
2017-06-05 16:52:03 -0500

    To Do corregido checkbox ok

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,39 +1,3 @@
-const TodoItem = (data,update) =>{
-  const todo = $('<div class="todo">');
-  const checkbox = $('<inpit type="checkbox">');
-  const span = $('<span>' + data.text+'</span>');
-  const remove = $('<button>Remove</button>');
-
-  todo.append(checkbox);
-  todo.append(span);
-  todo.append(remove);
-
-  checkbox.on('change',(e) => {
-    data.completed = !data.completed;
-    update();
-  });
-
-  remove.on('click',(e) => {
-    const idx = state.todos.map(x => x.test).indexOf(data.text);
-    state.todos.splice(idx,1);
-    update();
-  });
-
-  return todo;
-}
-
-const reRender = (todoList, completedList) => {
-  todoList.empty();
-  completedList.empty();
-  state.todos.forEach(todo =>{
-    if(!todo.completed){
-      todoList.append(TodoItem(todo,_ => { reRender(todoList,completedList);}));
-    } else {
-      completedList.append(TodoItem(todo,_ =>{ reRender(todoList, completedList);}));
-    }
-  });
-}
-
 const Todo = () =>{
   const parent = $('<div class="white-card"></div>');
   const input = $('<input id="input-item" type="text" placeholder="Ingresa la tarea">');
@@ -65,3 +29,38 @@ const Todo = () =>{
 
   return parent;
 }
+
+const reRender = (todoList, completedList) => {
+  todoList.empty();
+  completedList.empty();
+  state.todos.forEach(todo =>{
+    if(!todo.completed){
+      todoList.append(TodoItem(todo,_ => { reRender(todoList,completedList);}));
+    } else {
+      completedList.append(TodoItem(todo,_ =>{ reRender(todoList, completedList);}));
+    }
+  });
+}
+
+const TodoItem = (data,update) => {
+  const todo = $('<div class="todo">');
+  const checkbox = $('<input type="checkbox">');
+  const span = $('<span>'+data.text+'</span>');
+  const remove = $('<button>Remove</button>');
+
+  todo.append(checkbox);
+  todo.append(span);
+  todo.append(remove);
+
+  checkbox.on('change', (e) => {
+    data.completed = !data.completed;
+    update();
+  });
+
+  remove.on('click', (e) => {
+    const idx = state.todos.map( x => x.text ).indexOf(data.text);
+    state.todos.splice(idx,1);
+    update();
+  });
+  return todo;
+};  
